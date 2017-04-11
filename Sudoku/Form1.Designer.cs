15f31ad1c4ef15812d08d3d588236e78d7d330cd
ThomasGille <totogille@gmail.com>
2017-04-11 11:12:24 +0200

    Changed Title

--- Sudoku/Form1.Designer.cs
+++ Sudoku/Form1.Designer.cs
@@ -34,10 +34,12 @@
             // label1
             // 
             this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(0, 0);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(46, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // Form1
             // 
@@ -45,8 +47,9 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(500, 500);
             this.Controls.Add(this.label1);
-            this.Name = "Sudoku";
+            this.Name = "Form1";
             this.Text = "Sudoku - GILLE Thomas, MASSON Yohan";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
